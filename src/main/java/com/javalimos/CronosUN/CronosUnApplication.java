f91ea528a76ef4bc05a54ee3f6180928c4f746d5
AD <ing.alexis.duque@gmail.com>
2020-11-02 17:50:08 -0500

    Primer commit

---
+++ src/main/java/com/javalimos/CronosUN/CronosUnApplication.java
@@ -0,0 +1,13 @@
+package com.javalimos.CronosUN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronosUnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronosUnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javalimos/CronosUN/CronosUnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalimos.CronosUN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronosUnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
